21f71a85ea98f3eca52fc85f789369e1cbb2754f
Matej Kafka <git@a-n.cc>
2022-03-27 02:03:24 +0300

    Extend PSDirectories

--- CustomModules/PSDirectories/PSDirectories.psm1
+++ CustomModules/PSDirectories/PSDirectories.psm1
@@ -20,11 +20,21 @@ function Set-PSDataRoot {
 
 function Get-PSDataPath {
 	[CmdletBinding()]
-	param([Parameter(Mandatory)][string]$Name, [switch]$Directory, [switch]$NoCreate)
+	param([Parameter(Mandatory)][string]$Name, [switch]$Directory, [switch]$NoCreate, [string]$DefaultContentPath)
+
+	if ($DefaultContentPath) {
+		if (-not (Test-Path $DefaultContentPath)) {
+			throw "-DefaultContentPath path must exist: $DefaultContentPath"
+		}
+		if (-not (Test-Path $DefaultContentPath -Type $(if ($Directory) {"Container"} else {"Leaf"}))) {
+			# TODO: better error messages
+			throw "-DefaultContentPath has incorrect type (file/directory) - does not match the -Directory switch value"
+		}
+	}
 
 	if (-not $script:DataRoot) {
 		throw "Get-PSDataPath: PSDataRoot not set, should be initialized in `$PROFILE."
-		}
+	}
 
 	$Path = Join-Path $DataRoot $Name
 
@@ -35,10 +45,16 @@ function Get-PSDataPath {
 	if (-not $Directory) {
 		if (Test-Path -Type Leaf $Path) {
 			return Get-Item $Path
+		} elseif ($DefaultContentPath) {
+			Copy-Item $DefaultContentPath $Path
+			return Get-Item $Path
 		}
 		return New-Item $Path
 	} else {
 		if (Test-Path -Type Container $Path) {
+			return Get-Item $Path
+		} elseif ($DefaultContentPath) {
+			Copy-Item -Recurse $DefaultContentPath $Path
 			return Get-Item $Path
 		}
 		return New-Item -Type Directory $Path
