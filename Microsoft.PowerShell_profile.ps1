24f7bf56cde1e0f7dccf5665c87262a464870971
Matej Kafka <git@a-n.cc>
2022-07-18 15:27:47 +0200

    Update profile entry point to allow loading full profile manually

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,17 @@
-<# Entry point to my profile. #>
+<# Entry point to my profile, automatically loaded by pwsh on startup. #>
 Set-StrictMode -Version Latest
-
-
-# this file is symlinked from $PROFILE in my configuration, resolve the real target
-$PSCommandPath = Get-Item $PSCommandPath
-		| % {if ($null -ne $_.Target) {$_.Target} else {$_}}
-		| % {[IO.Path]::Combine($PSScriptRoot, $_)}
-		| Resolve-Path
-$PSScriptRoot = Split-Path $PSCommandPath
 
 # only load the full profile on Windows when running inside Windows Terminal or explicitly requested
 # change this if you don't use Windows Terminal
 if (-not $IsWindows -or (Test-Path Env:WT_SESSION) -or (Test-Path Env:PS_FULL_PROFILE)) {
-	Import-Module $PSScriptRoot\profile_full -DisableNameChecking
+	# this file ($PSCommandPath) is symlinked from $PROFILE in my configuration, resolve the real target
+	#  (.ResolvedTarget was added in v7.3.0-preview.2)
+	Import-Module -DisableNameChecking `
+		(Join-Path (Split-Path (Get-Item $PSCommandPath).ResolvedTarget) "profile_full")
+} else {
+	# function to load the full profile manually, if needed
+	function full-profile {
+		$ProfileDir = Split-Path (Get-Item $PSCommandPath).ResolvedTarget
+		Import-Module $ProfileDir\profile_full -DisableNameChecking
+	}
 }
