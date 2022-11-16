f775dbf05eba610038ba2217ac872f6dc7d4ccef
Matej Kafka <git@a-n.cc>
2022-11-16 17:37:47 +0100

    profile: Update entry point env var checks

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,14 @@
 <# Entry point to my profile, automatically loaded by pwsh on startup. #>
+
 Set-StrictMode -Version Latest
 
-# only load the full profile on Windows when running inside Windows Terminal or explicitly requested
-# change this if you don't use Windows Terminal
-if (-not $IsWindows -or (Test-Path Env:WT_SESSION) -or (Test-Path Env:PS_FULL_PROFILE)) {
+# only load the full profile on Windows when explicitly requested or running inside one of specific terminals
+# change this if you want the profile to load in other terminals
+if (-not $IsWindows `
+		-or [Environment]::GetEnvironmentVariable("PS_FULL_PROFILE") <# manual override #> `
+		-or [Environment]::GetEnvironmentVariable("WT_SESSION") <# Windows Terminal #> `
+		-or [Environment]::GetEnvironmentVariable("TERMINAL_EMULATOR") -eq "JetBrains-JediTerm" <# JetBrains IDE terminal #> `
+		) {
 	# this file ($PSCommandPath) is symlinked from $PROFILE in my configuration, resolve the real target
 	#  (.ResolvedTarget was added in v7.3.0-preview.2)
 	Import-Module -DisableNameChecking `
