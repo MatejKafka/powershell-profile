7295e5cb90d7f1f949b9cd3a28be445b0a4ab559
Matej Kafka <git@a-n.cc>
2024-05-19 04:14:26 +0200

    -

--- UnmaintainedModules/Github-Api/Github-Api.psm1
+++ UnmaintainedModules/Github-Api/Github-Api.psm1
@@ -1,18 +1,38 @@
-function Get-GithubRelease {
+function Get-GitHubRelease {
+	[CmdletBinding()]
 	param(
-			<# Repository name, including org (e.g. "PowerShell/Powershell") #>
-			[Parameter(Mandatory)]
-			[string]
-		$RepositoryName
+			[Parameter(Mandatory, ValueFromPipeline)]
+			[ValidatePattern("^[^/\s]+/[^/\s]+$")]
+			[string[]]
+		$Repository,
+			### Retrieves tags instead of releases.
+			[switch]
+		$Tags
 	)
-	if ($RepositoryName -notlike "*/*") {
-		# if name doesn't have /, assume the Organization and repo name are the same
-		$RepositoryName = "$RepositoryName/$RepositoryName"
-	}
 
-	$Uri = "https://api.github.com/repos/$RepositoryName/releases"
-	Write-Verbose "Listing GitHub releases for '$RepositoryName'... (URL: $Uri)"
-	# -FollowRelLink automatically goes through all pages to get older releases
-	# | % {$_} flattens the pages to single linear stream of releases
-	return Invoke-RestMethod -Uri $Uri -FollowRelLink | % {$_}
+	process {
+		foreach ($r in $Repository) {
+			$Endpoint = if ($Tags) {"tags"} else {"releases"}
+			$Url = "https://api.github.com/repos/$r/$Endpoint"
+			Write-Verbose "Listing GitHub releases for '$r'... (URL: $Url)"
+
+			try {
+				# piping through Write-Output enumerates the array returned by irm into individual values
+				#  (see https://github.com/PowerShell/PowerShell/issues/15280)
+				# -FollowRelLink automatically goes through all pages to get older releases
+				Invoke-RestMethod -UseBasicParsing -FollowRelLink $Url | Write-Output
+			} catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+				$e = $_.Exception
+				if ($e.StatusCode -eq 404) {
+					throw "Cannot list $Endpoint for '$r', GitHub repository does not exist."
+				} elseif ($e.StatusCode -eq 403 -and $e.Response.ReasonPhrase -eq "rate limit exceeded") {
+					$Limit = try {$e.Response.Headers.GetValues("X-RateLimit-Limit")} catch {}
+					$LimitMsg = if ($Limit) {" (at most $Limit requests/hour are allowed)"}
+					throw "Cannot list $Endpoint for '$r', GitHub API rate limit exceeded$LimitMsg."
+				} else {
+					throw
+				}
+			}
+		}
+	}
 }
