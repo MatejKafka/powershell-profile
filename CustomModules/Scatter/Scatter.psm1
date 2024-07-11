af7c7b96791e1fc0954233033b1e8a09401aa686
Matej Kafka <git@a-n.cc>
2024-07-12 01:36:17 +0200

    CustomModules/Scatter: Add more customization parameters

--- CustomModules/Scatter/Scatter.psm1
+++ CustomModules/Scatter/Scatter.psm1
@@ -6,21 +6,41 @@ function Out-ScatterPlot {
 	[Alias("scatter")]
 	param(
 		[Parameter(ValueFromPipeline)]$InputObject,
+		[string]$Title,
+		[string]$LabelX,
+		[string]$LabelY,
 		[switch]$Line,
-		[switch]$NoPoint
+		[switch]$NoPoint,
+		[switch]$YFromZero
 	)
 
 	begin {
 		$Args = @()
-		if ($Line) {$Args += ("-l")}
+		if ($Title) {$Args += @("-t", $Title)}
+		if ($LabelX) {$Args += @("-x", $LabelX)}
+		if ($LabelY) {$Args += @("-y", $LabelY)}
+
+		if ($Line) {$Args += @("-l")}
 		if ($NoPoint) {$Args += @("-p")}
+		if ($YFromZero) {$Args += @("-0")}
 
 	    # forward input to the Python script
 	    $Pipeline = {python $PSScriptRoot/scatter.py @Args}.GetSteppablePipeline($myInvocation.CommandOrigin)
 	    $Pipeline.Begin($PSCmdlet)
 	}
 
-	process {$Pipeline.Process($_)}
-	end {$Pipeline.End()}
+	process {
+		$Index = $_ | select Index | % Index
+		$Value = $_ | select Item | % Item
+		if ($null -ne $Index -and $null -ne $Value) {
+			$Pipeline.Process("$Index,$Value")
+		} else {
+			$Pipeline.Process([string]$_)
+		}
+	}
+
+	end {
+		$Pipeline.End()
+	}
 }
 
--- CustomModules/Scatter/scatter.py
+++ CustomModules/Scatter/scatter.py
@@ -11,20 +11,38 @@ import re
 import getopt
 
 marker = '.'
-opts, args = getopt.getopt(sys.argv[1:], 'lp')
+title = None
+x_label = None
+y_label = None
+start_y_at_zero = False
+opts, args = getopt.getopt(sys.argv[1:], 'lp0t:x:y:')
 for o, a in opts:
-    if o == '-p':
-        marker = ''
-    if o == '-l':
-        marker += '-'
+    if o == '-p': marker = ''
+    if o == '-l': marker += '-'
+    if o == '-t': title = a
+    if o == '-x': x_label = a
+    if o == '-y': y_label = a
+    if o == '-0': start_y_at_zero = True
 
 
+show_legend = False
 x = []
 y = []
+data_label = None
 
 i = 0
 for line in sys.stdin:
-  vals = [float(n.strip()) for n in line.strip().split(",")]
+  line = line.strip()
+  if line.startswith("="):
+    if len(x) > 0:
+      plt.plot(x, y, marker, label=data_label)
+    x = []
+    y = []
+    data_label = line[1:].strip()
+    show_legend = True
+    continue
+
+  vals = [float(n.strip()) for n in line.split(",")]
   if len(vals) == 1:
     x.append(i)
     y.append(vals[0])
@@ -33,7 +51,17 @@ for line in sys.stdin:
     y.append(vals[1])
   i += 1
 
-plt.plot(x, y, marker)
+if len(x) > 0:
+  plt.plot(x, y, marker, label=data_label)
+
+if show_legend:
+  plt.legend()
+if start_y_at_zero:
+  plt.ylim(bottom=0)
+plt.title(title)
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+
 plt.minorticks_on()
 plt.grid(which='both', color='#eee')
 plt.show()
